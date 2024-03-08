e27dbcd3de2fb6a3c0436cc96110b86c4df3dcc3
WolfShado <125580234+WolfShado@users.noreply.github.com>
2024-03-08 20:55:58 +0000

    Add files via upload

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    if (typeof range !== 'string') {
+      throw TypeError('Range must be a string');
+    }
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
